86f49e44fb3804e898e281e3ba1a26256bb733bd
fabyaanusakti <064002300043@std.trisakti.ac.id>
2025-05-26 23:22:58 +0700

    Deployment

--- core/views.py
+++ core/views.py
@@ -4,22 +4,17 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def chat_view(request):
-    if 'chat' not in request.session:
-        request.session['chat'] = []
+
+    response = None
+    prompt = None
 
     if request.method == 'POST':
         prompt = request.POST.get('prompt', '')
-
-        if prompt:
-            response = service.response(prompt)
-            request.session['chat'].append({
-                'user': prompt,
-                'gurt': response
-            })
-            request.session.modified = True
+        response = service.response(prompt)
 
     context = {
-        'chat': request.session.get('chat', [])
+        'response': response,
+        'prompt': prompt,
     }
 
     return render(request, 'index.html', context)
