f7afdef6bdd5d6f14cbc2a698943208b3ad5d567
fabyaanusakti <064002300043@std.trisakti.ac.id>
2025-05-27 00:16:05 +0700

    Deployment

--- base/wsgi.py
+++ base/wsgi.py
@@ -1,18 +1,4 @@
-"""
-WSGI config for base project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-# import sys
-
-# project_home = '/home/faizbyaan/base'
-# if project_home not in sys.path:
-#     sys.path.insert(0, project_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
