c0d24cc833153ae3c5260738e71cdcabb81f9ede
fabyaanusakti <064002300043@std.trisakti.ac.id>
2025-05-26 21:46:18 +0700

    First Commit

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3$t4ucjom(4e-wco#k5&oj6xjy@x5r84+m!g^*k77q(rw%gq5q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['faizbyaan.pythonanywhere.com']
 
 GEMINI_API_KEY = ''
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
